4c0043e7dd80adc9d5e7ba365a2f53cd9307429d
StevenLiu <stevenliu986@gmail.com>
2025-08-03 23:36:39 +1000

    update

---
+++ 01-java-basic/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: ${NAME}
+ * Package: ${PACKAGE_NAME}
+ * Description:
+ *
+ * @Author: Steven_LIU
+ * @Create: 2025-08-03
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
