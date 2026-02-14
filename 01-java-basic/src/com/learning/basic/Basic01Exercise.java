667df3a4c4a04ce05ae85ce43aa45c2805edeb40
StevenLiu <stevenliu986@gmail.com>
2026-02-14 18:19:48 +1100

    打印菱形，添加一种适用于Java11以下的方法

--- 01-java-basic/src/com/learning/basic/Basic01Exercise.java
+++ 01-java-basic/src/com/learning/basic/Basic01Exercise.java
@@ -10,13 +10,44 @@ package com.learning.basic;
  */
 public class Basic01Exercise {
 
-    // 打印一个菱形
+    // 打印一个菱形，此方法适用于Java11及以上版本
     static void main() {
         for (int i = 1; i <= 4; i++) {
             System.out.println(" ".repeat(4 - i) + "*".repeat(2 * i - 1));
         }
         for (int i = 3; i >= 1; i--) {
             System.out.println(" ".repeat(4 - i) + "*".repeat(2 * i - 1));
+        }
+    }
+
+    // 另一种方法
+    void printDiamondPattern(){
+        int n = 4; // 菱形上半部分的高度（包括中间行）
+
+        // 1. 打印上半部分（1 到 4 行）
+        for (int i = 1; i <= n; i++) {
+            // 打印空格：每行空格数 = 总行数 - 当前行号
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // 打印星号：每行星号数 = 2 * 当前行号 - 1
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 2. 打印下半部分（剩余的 3 行）
+        for (int i = n - 1; i >= 1; i--) {
+            // 打印空格
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // 打印星号
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
