f12666ab77452f8d0aa0b1ad76419390d45afd11
StevenLiu <stevenliu986@gmail.com>
2026-02-21 21:03:59 +1100

    format

--- 01-java-basic/src/com/learning/basic/BasicExercise02.java
+++ 01-java-basic/src/com/learning/basic/BasicExercise02.java
@@ -19,7 +19,7 @@ public class BasicExercise02 {
     }
 
     // 从键盘上接收输入的正整数或负整数，只计算正整数的和，如果输入为0，则退出程序
-    void calculatePositiveInteger(){
+    void calculatePositiveInteger() {
         Scanner scanner = new Scanner(System.in);
         int input, value = 0;
         do {
@@ -34,22 +34,22 @@ public class BasicExercise02 {
     }
 
     // 打印出从startNum-endNum内的所有奇数，但跳过以3结尾的数
-    void printOddNumber(int startNum, int endNum){
+    void printOddNumber(int startNum, int endNum) {
         // 参数验证
         if (startNum > endNum) {
             System.out.println("起始值不能大于结束值");
             return;
         }
 
-        for (int i = (startNum % 2 == 0)? startNum + 1:startNum; i <= endNum; i += 2) {
-            if ( i % 10 != 3) {
+        for (int i = (startNum % 2 == 0) ? startNum + 1 : startNum; i <= endNum; i += 2) {
+            if (i % 10 != 3) {
                 System.out.println(i);
             }
         }
     }
 
     // 韩信点兵，3人一组余2人，5人一组余3人，7人一组余4人，需要至少多少人？
-    void countSoldier(){
+    void countSoldier() {
         int minAmountSoldier = 1;
         while (true) {
             if (minAmountSoldier % 3 == 2 && minAmountSoldier % 5 == 3 && minAmountSoldier % 7 == 4) {
@@ -61,7 +61,7 @@ public class BasicExercise02 {
     }
 
     // 步长优化版本一，原理： 既然要满足 n % 3 == 2，那么 n 只能是 2, 5, 8, 11...（每次+3）
-    void optimisedCountSoldier(){
+    void optimisedCountSoldier() {
         // 从满足第一个条件的数开始：2, 5, 8, 11...
         int minAmountSoldier = 2;
 
@@ -75,7 +75,7 @@ public class BasicExercise02 {
     }
 
     // 步长优化版本二，原理： 同时满足 n % 3 == 2 和 n % 5 == 3 的数，公差为 15
-    void optimisedCountSoldier2(){
+    void optimisedCountSoldier2() {
         int minAmountSoldier = 8;
 
         while (true) {
