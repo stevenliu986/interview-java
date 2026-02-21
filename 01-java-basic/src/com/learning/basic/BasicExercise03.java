e142f72445d1f71dd4dca925b5979626de2d46d5
StevenLiu <stevenliu986@gmail.com>
2026-02-21 21:47:58 +1100

    新练习

---
+++ 01-java-basic/src/com/learning/basic/BasicExercise03.java
@@ -0,0 +1,34 @@
+package com.learning.basic;
+
+/**
+ * ClassName: BasicExercise03
+ * Package: com.learning.basic
+ * Description:
+ *
+ * @Author: Steven_LIU
+ * @Create: 21/2/2026
+ */
+public class BasicExercise03 {
+    static void main() {
+        BasicExercise03 be03 = new BasicExercise03();
+        be03.calculatePeachAmount();
+        System.out.println(be03.peachCount(1));
+    }
+
+    // 猴子第1天摘了若干个桃子，当即吃了一半又一个，第2天又吃了一半又一个，以后每天吃剩下的一半又一个，到第10天的时候发现只剩下了一个桃子，问一共有多少个桃子
+    void calculatePeachAmount() {
+        int amount = 1;
+        for (int i = 0; i < 9; i++) {
+            amount = (amount + 1) * 2;
+        }
+        System.out.println(amount);
+    }
+
+    // 猴子吃桃 - 递归解法
+    int peachCount(int day) {
+        if (day == 10) {
+            return 1;
+        }
+        return (peachCount(day + 1) + 1) * 2;
+    }
+}
