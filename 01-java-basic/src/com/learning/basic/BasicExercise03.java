0c77a08f5846fe4bb310551b07265e77c01ec468
StevenLiu <stevenliu986@gmail.com>
2026-02-22 19:21:33 +1100

    新练习

--- 01-java-basic/src/com/learning/basic/BasicExercise03.java
+++ 01-java-basic/src/com/learning/basic/BasicExercise03.java
@@ -13,6 +13,7 @@ public class BasicExercise03 {
         BasicExercise03 be03 = new BasicExercise03();
         be03.calculatePeachAmount();
         System.out.println(be03.peachCount(1));
+        be03.monkAmount();
     }
 
     // 猴子第1天摘了若干个桃子，当即吃了一半又一个，第2天又吃了一半又一个，以后每天吃剩下的一半又一个，到第10天的时候发现只剩下了一个桃子，问一共有多少个桃子
@@ -30,5 +31,15 @@ public class BasicExercise03 {
             return 1;
         }
         return (peachCount(day + 1) + 1) * 2;
+    }
+
+    // 100个和尚吃了100个馒头，100个和尚有大和尚也有小和尚，其中大和尚一个人可以吃3个馒头，3个小和尚吃1个馒头，问大和尚和小和尚各有多少？
+    void monkAmount() {
+        int i = 2;
+        while ((3 * i + (100 - i) / 3 != 100)) {
+            i++;
+        }
+        System.out.println("大和尚的人数是： " + i);
+        System.out.println("小和尚的人数是： " + (100 - i));
     }
 }
